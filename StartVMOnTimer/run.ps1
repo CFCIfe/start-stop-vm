c392995fd7ba3c72fdd17abbf560fb192229dfc3
CFCIFE <aboludepeter@gmail.com>
2022-10-21 13:26:31 +0000

    Code. Finish

--- StartVMOnTimer/run.ps1
+++ StartVMOnTimer/run.ps1
@@ -9,11 +9,11 @@ $vms = Get-AzVM -ResourceGroupName $RgName -Status
 
 $vms | ForEach-Object -Parallel {
    if ($_.PowerState -eq 'VM running'){
-      Stop-AzVM -Name $_.Name -ResourceGroupName $_.ResourceGroupName -Confirm:$false -Force
-      Write-Warning "Stop VM - $($_.Name)"
+      Write-Warning "Could not Stop VM - $($_.Name) as it is running"
    }
    elseif ($_.PowerState -eq 'VM deallocated') {
-      Write-Warning "I could not Start VM - $($_.Name) because it is currently running"
+      Start-AzVM -Name $_.Name -ResourceGroupName $_.ResourceGroupName
+      Write-Warning "Started VM - $($_.Name)"
    }
    Start-Sleep 1
 } -ThrottleLimit 10 #Number of VM's
